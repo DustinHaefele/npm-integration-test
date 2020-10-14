c38f51065371a22bc5628430be292fefadcb2351
dustin22 <dustin@auditdeploy.com>
2020-10-14 09:47:42 -0400

    Added how to install on your project in the readme

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const printName = require("printName");
+const Utils = require("utilsLibrary");
+
+
 
 function run(name) {
+  const testObject = {
+    key1: {
+      key2: {
+        keyToFind: "You Found Me"
+      }
+    }
+  }
   printName.printMyName(name);
+  const keyObj = Utils.findKeyInObject(testObject, "keyToFind");
+  console.log(keyObj);
   return;
 }
 
